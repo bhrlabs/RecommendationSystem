0f6278521444787dcc482f389ef181d09e0f6ec8
bks2009 <bharat.ku.singh@gmail.com>
2016-03-25 22:24:06 -0700

    Users data

--- Utilities/csv_filter.py
+++ Utilities/csv_filter.py
@@ -1,12 +1,38 @@
+
 __author__ = 'Bharat'
 
 import csv
+from sets import Set
 
 
-with open('Restaurant.csv', 'rb') as inp, open('phx.csv', 'wb') as out:
-    writer = csv.writer(out)
-    count = 0
-    for row in csv.reader(inp):
-        if row[58] == 'Phoenix' or row[58] == 'Pheonix' or row[58] == 'Phoenix Sky Harbor Center' or count == 0:
-            writer.writerow(row)
-            count += 1
+def get_phx_restaurants():
+    with open('Restaurant.csv', 'rb') as inp, open('phx.csv', 'wb') as out:
+        writer = csv.writer(out)
+        count = 0
+        for row in csv.reader(inp):
+            if row[58] == 'Phoenix' or row[58] == 'Pheonix' or row[58] == 'Phoenix Sky Harbor Center' or count == 0:
+                writer.writerow(row)
+                count += 1
+
+
+def get_phx_user():
+    with open('Restaurant_Reviewers_Phoenix.csv', 'rb') as inp1,open('Files/yelp_academic_dataset_user.csv') as inp2, open('phx_users.csv', 'wb') as out:
+        writer = csv.writer(out)
+
+        phx_users = Set([])
+        for row in csv.reader(inp1):
+            phx_users.add(row[0])
+
+        count = 0
+        for row in csv.reader(inp2):
+            if row[16] in phx_users or count == 0:
+                writer.writerow(row)
+                count += 1
+
+    print count
+
+
+
+if __name__ == '__main__':
+    #get_phx_restaurants()
+    get_phx_user()
