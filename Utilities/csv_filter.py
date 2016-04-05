60cbc6c78d95624c768a3b56286e7e075816116d
bks2009 <bharat.ku.singh@gmail.com>
2016-04-04 23:51:02 -0700

    Users for 500 restaurants

--- Utilities/csv_filter.py
+++ Utilities/csv_filter.py
@@ -16,16 +16,21 @@ def get_phx_restaurants():
 
 
 def get_phx_user():
-    with open('Restaurant_Reviewers_Phoenix.csv', 'rb') as inp1,open('Files/yelp_academic_dataset_user.csv') as inp2, open('phx_users.csv', 'wb') as out:
+
+    user_file = 'C:\Bharat\Masters Material\GIT Repo\RecommendationSystem\Data Files\Phoenix\PHX_Users.csv'
+    review_file = 'C:\Bharat\Masters Material\GIT Repo\RecommendationSystem\Data Files\Phoenix\Top500businessInPhx\\reviews_top_500_phx.csv'
+    with open(review_file, 'rb') as inp1,open(user_file) as inp2, open('phx_users_500.csv', 'wb') as out:
         writer = csv.writer(out)
 
         phx_users = Set([])
         for row in csv.reader(inp1):
-            phx_users.add(row[0])
+            print row[1]
+            phx_users.add(row[1])
 
         count = 0
         for row in csv.reader(inp2):
             if row[16] in phx_users or count == 0:
+                print row[16]
                 writer.writerow(row)
                 count += 1
 
